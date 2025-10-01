5556728f95f4037e5e8732232c216649e1ea8092
raketa0 <rychkovalex04@gmail.com>
2025-10-02 01:30:06 +0300

    Create Reverse

---
+++ docs/competitors/С#/Reverse/Programm.cs
@@ -0,0 +1,22 @@
+﻿
+namespace ProgramName
+{
+    public static class Programm
+    {
+        private static void Main(string[] args)
+        {
+            string? str = Console.ReadLine();
+            if (!string.IsNullOrEmpty(str))
+            {
+                char[] chars = str.ToCharArray();
+                Array.Reverse(chars);
+                string reversed = new string(chars);
+                Console.WriteLine($"Реверсированная строка: {reversed}");
+            }
+            else
+            {
+                Console.WriteLine("Строка пуста или не введена.");
+            }
+        }
+    }
+}
