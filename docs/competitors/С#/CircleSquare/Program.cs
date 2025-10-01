fc77e881ba8aa8c9fd7743fa1ae2a80e9c21fb8d
raketa0 <rychkovalex04@gmail.com>
2025-10-01 23:35:58 +0300

    CircleSquare реализовал C#

---
+++ docs/competitors/С#/CircleSquare/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ProgramMain
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус круга:");
+            double radiusCircle = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Площадь круга: {Math.PI * radiusCircle * radiusCircle:F2}");
+        }
+    }
+}
