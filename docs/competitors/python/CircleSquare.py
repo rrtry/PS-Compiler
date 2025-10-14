c8ac259edebebbd3349030c2055c3b69587d3ef0
raketa0 <rychkovalex04@gmail.com>
2025-10-15 01:17:25 +0300

    o

--- docs/competitors/python/CircleSquare.py
+++ docs/competitors/python/CircleSquare.py
@@ -1,6 +1,9 @@
 import math
 radiusCircle = float(input("Введите радиус круга: "))
 if radiusCircle > 0:
-    print("Площадь круга: ", math.pi * radiusCircle * radiusCircle)
+    print("Площадь круга: ", 
+          math.pi * radiusCircle * 
+          radiusCircle)
+    
 else:
     print("Радиус должен быть положительным числом")
